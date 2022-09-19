7534417e41b641ed8fd34435e0c403b40e2858a8
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-18 23:15:15 -0700

    set root for signed in users and unsigned in users

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'post/index'
-  get 'post/show'
-  get 'post/new'
-  get 'post/create'
-  get 'post/edit'
-  get 'post/update'
-  get 'post/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root "post#index"
+
+  authenticated :user do
+    root "post#index", as: :authenticated_root
+  end
+
+  root "static_pages#home"
 end
