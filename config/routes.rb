5d33c9c710cc525dd9439667d20e7d6d71daa25e
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-18 22:44:11 -0700

    add MVC for Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/show'
+  get 'post/new'
+  get 'post/create'
+  get 'post/edit'
+  get 'post/update'
+  get 'post/destroy'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "devise/sessions#new"
 end
