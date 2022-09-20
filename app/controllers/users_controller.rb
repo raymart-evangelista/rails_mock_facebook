6b8ea3359eaf736ff12d6f38619e9e47409e2fec
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-20 14:22:58 -0700

    fix views and controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
-    @addable_friends = @user.addable_users
+    # @addable_friends = @user.addable_users
   end
 
   private
