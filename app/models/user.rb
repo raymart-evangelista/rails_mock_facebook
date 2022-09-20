56bf532ac8aeab3969d30f338825fa0c857c02a3
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-19 17:56:48 -0700

    add username attribute to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   has_many :posts
   has_many :friends
   devise :database_authenticatable, :registerable,
