0c846f2d1a1300e881878f0a27027e65641c6bf6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-20 21:22:40 -0700

    fix User#friends

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
   # belongs_to :friends, class_name: "User", foreign_key: :friend_id
-  belongs_to :friend_a, class_name: :User
-  belongs_to :friend_b, class_name: :User
+  belongs_to :friend_a, foreign_key: :friend_a_id, class_name: :User
+  belongs_to :friend_b, foreign_key: :friend_b_id, class_name: :User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,19 +7,33 @@ class User < ApplicationRecord
   # has_many :friendship_list, foreign_key: :user_id, class_name: "Friendship"
   # has_many :friends, through: :friendship_list
 
-  has_many :friendship_list, foreign_key: :friend_a_id, class_name: :Friendship
-  has_many :friends, through: :friendship_list
+  # has_many :friendship_list, foreign_key: :friend_a_id, class_name: :Friendship
+  # has_many :friends, through: :friendship_list
+
+  # User.all_friends(user)
+  # scope :all_friends, ->(user) { Friendship.where("friend_a_id = ? OR friend_b_id = ?", user.id, user.id)}
+
+  def friends
+    Friendship.where("friend_a_id = ? OR friend_b_id = ?", self.id, self.id)
+  end
+
+  def addable_friends
+    # an addable friend is someone who meets the following criteria:
+      # - the addable friend is not a current friend
+      # - the addable friend is not in friend requests
+        # - specifically, the addable friend is not a requestor
+        # - specifically, the addable friend is not a receiver
+  end
+
+  # has_many :friendships, foreign_key: :friend_a_id, class_name: :Friendship
+  # has_many :friendships, -> { where("friend_a_id = ? OR friend_b_id = ?", user.id, user.id) }
+  # has_many :friends, through: :friendships, source: :Friendship
+
   # has_many :friendships, ->(user) { where("friend_a_id = ? OR friend_b_id = ?", user.id, user.id) }
   # has_many :friends, through: :friendships
 
   has_many :friend_requests_as_requestor, foreign_key: :requestor_id, class_name: :FriendRequest
   has_many :friend_requests_as_receiver, foreign_key: :receiver_id, class_name: :FriendRequest
-
-  # scope, add a new friend where Users not in friends and not current user
-
-  def addable_users
-    User.where.not(id: self.friends.ids.push(self.id))
-  end
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
