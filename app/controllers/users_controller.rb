1e47b7162e6db59e2aed5c038b9ad2900226b771
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-21 20:18:37 -0700

    add logic for creating friend request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :toggle_add_friend]
   def show
-    @user = current_user
-
     # @addable_friends = @user.addable_users
   end
 
   def toggle_add_friend
+    # binding.pry
+    # get the user to add
+    @person = User.find(params[:id])
+    # create a sent request in FreindRequest
+    FriendRequest.create!(requestor_id: @user.id, receiver_id: @person.id)
+    # redirect, page should show that the Friend request was sent
+    redirect_to user_url(@user.username), notice: "Friend request sent."
   end
+
   private
+
+  def set_user
+    @user = current_user
+  end
 end
