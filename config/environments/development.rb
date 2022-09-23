8b7ad4897da7c6e7d688c1b2f1829999c8bdbbd6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-09-22 22:43:12 -0700

    tailwind issues

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.reload_classes_only_on_change = false
+
 end
