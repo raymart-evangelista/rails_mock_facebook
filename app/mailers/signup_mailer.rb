3170db8cefbb342ff44028ce39b8a31b7193861e
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-10-03 22:45:35 -0700

    create text and html mailer signup_email

--- app/mailers/signup_mailer.rb
+++ app/mailers/signup_mailer.rb
@@ -1,2 +1,9 @@
 class SignupMailer < ApplicationMailer
+  default from: 'signupnotifications@mock.com'
+
+  def signup_email
+    @user = current_user
+    @url  = 'http://example.com/login'
+    mail(to: @user.email, subject: 'Welcome to mock facebook. Thank you for signing up.')
+  end
 end
